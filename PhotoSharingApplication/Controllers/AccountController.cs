12d8cda868d2f41dd9643ac85070d32434b0a2d4
Jens Lindhardt <jli@itucation.dk>
2015-03-30 11:12:34 +0200

    Modul 11 ex 2

---
+++ PhotoSharingApplication/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using PhotoSharingApplication.Models;
+
+namespace PhotoSharingApplication.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(Login model, string returnUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (Membership.ValidateUser(model.UserName, model.Password))
+                {
+                    FormsAuthentication.SetAuthCookie(model.UserName, model.RememberMe);
+                    if (Url.IsLocalUrl(returnUrl))
+                    {
+                        return Redirect(returnUrl);
+                    }
+                    return RedirectToAction("Index", "Home");
+                }
+                ModelState.AddModelError( "", "the User name or password is incorrect!" );
+            }
+            return View("Login", model);
+        }
+
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+
+            return RedirectToAction("Index","Home");
+        }
+
+        public ActionResult Register()
+        {
+            return View("Register");
+        }
+
+        [HttpPost]
+        public ActionResult Register(Register model)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var newUser = Membership.CreateUser(model.UserName, model.Password);
+                    FormsAuthentication.SetAuthCookie(model.UserName,false);
+                    return RedirectToAction("Index","Home");
+                }
+                catch (MembershipCreateUserException e)
+                {
+                    ModelState.AddModelError("Registration Error", e.StatusCode.ToString() );
+                }
+            }
+            return View("Register", model);
+        }
+    }
+}
---
+++ PhotoSharingApplication/Models/AccountModelClasses.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+
+namespace PhotoSharingApplication.Models
+{
+    public class UsersContext:DbContext
+    {
+        public UsersContext() : base("PhotoAppServices") { }
+    }
+
+    public class Login
+    {
+        [DisplayName("User name")]
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DisplayName("Remember me?")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class Register
+    {
+        [DisplayName("User name")]
+        [Required]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+    }
+}
